c65e66f6eb30768965009bd2a87be11fcd7c2393
Samyog Dhital <dhitalsamyog123@gmail.com>
2023-02-08 03:06:19 +0545

    chore: bootstrap5 and scss on the project

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,8 +1,14 @@
+import React from "react";
 import { type AppType } from "next/dist/shared/lib/utils";
 
 import "../styles/globals.css";
+import "../styles/min.css";
 
 const MyApp: AppType = ({ Component, pageProps }) => {
+  React.useEffect(() => {
+    import("bootstrap/dist/js/bootstrap");
+  }, []);
+
   return <Component {...pageProps} />;
 };
 
